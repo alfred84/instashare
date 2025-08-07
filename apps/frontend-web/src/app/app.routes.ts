d759f817fdc45e59ac6a2c10dab621b19f294dc1
Alfredo Saltaren <alfredo840819@gmail.com>
2025-08-06 20:48:07 -0400

    feat: setting the initial frontend routes

--- apps/frontend-web/src/app/app.routes.ts
+++ apps/frontend-web/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    { 
+        path: 'login', 
+        loadComponent: () => import('./components/login/login').then(m => m.Login)
+    },
+    { 
+        path: 'register', 
+        loadComponent: () => import('./components/register/register').then(m => m.Register)
+    },
+    { 
+        path: 'dashboard', 
+        loadComponent: () => import('./components/dashboard/dashboard').then(m => m.Dashboard)
+    },
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: '**', redirectTo: '/dashboard' } 
+];
